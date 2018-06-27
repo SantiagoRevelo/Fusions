7ae134c1802daed2adb7e48b1ee79bc882197e2a
sreveloc <sreveloc@gmail.com>
2018-06-28 00:58:27 +0200

    updates

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -4,14 +4,18 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour {
 
-	public static GameManager instance = null; 
+	public static GameManager instance = null;
 
-	public Camera mainCamera;
+
+	public RectTransform GUI_TopContent;
+	public RectTransform GUI_BottomContent;
 
 	public BoxCollider2D topWall;
 	public BoxCollider2D rightWall;
 	public BoxCollider2D bottomWall;
 	public BoxCollider2D leftWall;
+
+	Camera mainCamera;
 
 	void Awake() {
 		if (instance == null) {
@@ -26,31 +30,37 @@ public class GameManager : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-		RelocateWalls ();
+		
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+		RelocateWalls ();
 	}
 
 	void RelocateWalls() {
-		topWall.transform.position = Vector3.zero;
+		topWall.transform.position = mainCamera.transform.position;
 		topWall.transform.localScale = Vector3.one;
-		topWall.size = new Vector2 (mainCamera.ScreenToWorldPoint (new Vector3 (Screen.width * 2f, 0f, 0f)).x, 1f);
-		topWall.offset = new Vector2 (0f, mainCamera.ScreenToWorldPoint (new Vector3 (0f, Screen.height, 0f)).y - 0.5f);
+		Vector2 screen = mainCamera.ScreenToWorldPoint (new Vector3 (Screen.width, Screen.height, 0f));
+		Vector2 topC =  mainCamera.ScreenToWorldPoint (new Vector3 (GUI_TopContent.sizeDelta.x, GUI_TopContent.sizeDelta.y));
 
-		rightWall.transform.position = Vector3.zero;
+		topWall.size = new Vector2 (Mathf.Abs(topC.x) * 2, Mathf.Abs(topC.y));
+		topWall.offset = new Vector2 (0f, (screen.y));
+
+
+
+		rightWall.transform.position = mainCamera.transform.position;
 		rightWall.transform.localScale = Vector3.one;
 		rightWall.size = new Vector2 (1f, mainCamera.ScreenToWorldPoint (new Vector3 (0f, Screen.height * 2f, 0f)).y);
 		rightWall.offset = new Vector2 (mainCamera.ScreenToWorldPoint (new Vector3 (Screen.width, 0f, 0f)).x + 0.5f, 0f);
 
-		bottomWall.transform.position = Vector3.zero;
+		bottomWall.transform.position = mainCamera.transform.position;
 		bottomWall.transform.localScale = Vector3.one;
-		bottomWall.size = new Vector2 (mainCamera.ScreenToWorldPoint (new Vector3 (Screen.width * 2f, 0f, 0f)).x, 1f);
-		bottomWall.offset = new Vector2 (0f, mainCamera.ScreenToWorldPoint (new Vector3 (0f, 0f, 0f)).y + 0.5f);
+		float bottomWallHeight = mainCamera.ScreenToWorldPoint (new Vector3 (Screen.width, GUI_BottomContent.rect.height)).y;
+		bottomWall.size = new Vector2 (mainCamera.ScreenToWorldPoint (new Vector3 (Screen.width * 2f, 0f, 0f)).x, bottomWallHeight);
+		bottomWall.offset = new Vector2 (0f, mainCamera.ScreenToWorldPoint (Vector3.zero).y + (bottomWallHeight * 0.5f));
 
-		leftWall.transform.position = Vector3.zero;
+		leftWall.transform.position = mainCamera.transform.position;
 		leftWall.transform.localScale = Vector3.one;
 		leftWall.size = new Vector2 (1f, mainCamera.ScreenToWorldPoint (new Vector3 (0f, Screen.height * 2f, 0f)).y);
 		leftWall.offset = new Vector2 (mainCamera.ScreenToWorldPoint (new Vector3 (0f, 0f, 0f)).x - 0.5f, 0f); 
